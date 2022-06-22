5e2f431fa2fe5eb93a61ca28e933a3dd458f0fdb
Ulisesx21 <75042766+Ulisesx21@users.noreply.github.com>
2022-06-22 07:18:27 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import './App.css';
+import { useState, useRef } from 'react';
+import Main from "./components/Main"
+import Verbos from "./components/Verbos"
+
+
+
+function App() {
+
+  let [input, setInput] = useState("")
+  let [state, setState] = useState(0)
+  let [count, setCount] = useState(0)
+  let [boolean, setBoolean] = useState(false)
+  let [counterState, setCounterState] = useState(true)
+  let [gameState, setGameState] = useState(true)
+
+  function handleChange(e) {
+
+    if (e.key == "Enter") {
+      if (gameState) {
+        if (e.target.value.toLowerCase() == Verbos[state].respuesta) {
+          if (state >= Verbos.length - 1) {
+            setInput("Finalizado")
+            e.target.value = ""
+            setGameState(false)
+          } else {
+            setState(state + 1)
+            setInput("")
+            setBoolean(false)
+            setCounterState(true)
+            e.target.value = ""
+          }
+        } else {
+          setInput("INCORRECTO")
+          e.target.value = ""
+        }
+      }
+    }
+  }
+
+  function handleClick(e) {
+    if (gameState) {
+      setBoolean(!boolean)
+      if (counterState) {
+        setCounterState(!counterState)
+        setCount(count + 1)
+      }
+    }
+  }
+
+
+  return (
+    <div className="App">
+      {count}
+      <h1 id="titulo">Los 100 verbos más usados en inglés</h1>
+      <Main input={input} handleClick={handleClick} handleChange={handleChange} Verbos={Verbos} count={state} boolean={boolean} gameState={gameState} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+
+
+export default function Main(props){
+    return(
+        <div className="container">
+            <h1>{props.Verbos[props.count].verbo}</h1>
+            <input className="input" onKeyDown={props.handleChange} placeholder="..."/>
+            <div className="show" onClick={props.handleClick}>
+                {props.boolean ? props.Verbos[props.count].respuesta : <img className="img" src="https://cdn-icons-png.flaticon.com/512/64/64943.png"></img>}
+            </div>
+            <h2 className="incorrecto">{props.input}</h2>
+        </div>
+    )
+}
