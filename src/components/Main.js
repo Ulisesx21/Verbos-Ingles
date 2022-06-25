161658f99aa71219a2038f82fcad77f932db21f9
Ulisesx21 <75042766+Ulisesx21@users.noreply.github.com>
2022-06-25 08:02:17 -0300

    mejoras varias, cambio de fuente

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@
 export default function Main(props){
     return(
         <div className="container">
-            <h1>{props.Verbos[props.count].verbo}</h1>
+            <h1 className="verbo-principal">{props.Verbos[props.count].verbo}</h1>
             <input className="input" onKeyDown={props.handleChange} placeholder="Escriba aqui..."/>
             <div className="show" onClick={props.handleClick}>
                 {props.boolean ? props.Verbos[props.count].respuesta : <img className="img" src="https://cdn-icons-png.flaticon.com/512/64/64943.png"></img>}
--- src/components/Visor.js
+++ src/components/Visor.js
@@ -3,7 +3,6 @@ export default function Visor(props){
         <div className="visor-container">
             <h1 className="porcentaje">{`${((props.completado*100)/props.arrlength).toFixed(0)}%`}</h1>
             <h1 className="visto"><img className="img-visto" src="https://cdn-icons-png.flaticon.com/512/64/64943.png"></img>{props.visor}</h1>
-        </div>
-        
+        </div>  
     )
 }
