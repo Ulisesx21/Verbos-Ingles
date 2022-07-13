b8d3eb3c9d8285f85cbfac01f683f87e048204c4
Ulisesx21 <75042766+Ulisesx21@users.noreply.github.com>
2022-07-13 04:26:26 -0300

    cambios dif

--- src/components/Dificulty.js
+++ src/components/Dificulty.js
@@ -4,11 +4,11 @@ export default function Dificulty(props){
         <h3>Dificultad</h3>
         <select onChange={props.handleDif}>
           <option value="easy">Facil</option>
-          <option value="m I.I">Media I.I</option>
-          <option value="m I.II">Media I.II</option>
-          <option value="m II.I">Media II.I</option>
-          <option value="m II.II">Media II.II</option>
-          <option value="m II.III">Media II.III</option>
+          <option value="m I.I">Verb I</option>
+          <option value="m I.II">Verb II</option>
+          <option value="m II.I">Adj I</option>
+          <option value="m II.II">Adj II</option>
+          <option value="m II.III">Adj III</option>
           <option value="hard">Dificil</option>
         </select>
       </div>
